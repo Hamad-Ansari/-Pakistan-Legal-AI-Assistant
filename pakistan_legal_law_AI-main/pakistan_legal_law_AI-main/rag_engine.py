562ec3b321e5ba41ab5900199c4a20722d06c110
Hammad Zahid <178669060+Hamad-Ansari@users.noreply.github.com>
2026-05-02 11:56:52 +0500

    Update rag_engine.py

--- pakistan_legal_law_AI-main/pakistan_legal_law_AI-main/rag_engine.py
+++ pakistan_legal_law_AI-main/pakistan_legal_law_AI-main/rag_engine.py
@@ -7,9 +7,10 @@ import pickle
 from pathlib import Path
 
 from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 import config
 
